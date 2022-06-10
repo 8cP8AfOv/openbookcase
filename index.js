d829481d948ee3c7a0bb02f3771b6f8d39b336a6
YY <cafesenzanome2005@yahoo.co.jp>
2022-06-10 09:34:25 +0900

    abc

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 /*-----------------------------------------------------------
 Postgres のテスト
 --------------------------------------------------------------*/
-/*
+
 var r = 'a';
 const { Client } = require('pg')
 
+/*
 const pg = new Client({
     user: 'postgres',
     host: '127.0.0.1',
@@ -12,6 +13,16 @@ const pg = new Client({
     password: 'postgres',
     port: 5432,
 })
+*/
+
+/*
+const pg = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+*/
+console.log(process.env.DATABASE_URL )
+
 
 const pg = new Client({
   connectionString: process.env.DATABASE_URL,
@@ -19,11 +30,13 @@ const pg = new Client({
     rejectUnauthorized: false
   }
 });
-*/
+
+
 //--------------------------------------------------------------------------
-/*
+
 pg.connect();
 
+/*
 pg.query('SELECT * from users;', (err, result) => {
     if (err) throw err;
     	//r = JSON.stringify(result.rows[0]);
@@ -32,8 +45,9 @@ pg.query('SELECT * from users;', (err, result) => {
     	pg.end();
 });
 */
+
 // 現在時刻を取得
-/*
+
 pg.query('SELECT NOW()', (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
@@ -42,26 +56,33 @@ pg.query('SELECT NOW()', (err, res) => {
   // 接続終了
   pg.end();
 });
-*/
+
 //--------------------------------------------------------------------------
 
 var express = require('express');
 var app = express();
 const bodyParser = require('body-parser')
 const cors = require('cors')
+var path = require('path');
 
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors())
 app.use(express.static('./dist'))
+//app.use(express.static(path.join(__dirname, 'dist')));
 
 const helmet = require('helmet')
 app.use(helmet())
 
 const port = process.env.PORT ||  3000; //8080;
 
+var users = require('./routes/users');
+app.use('/users', users);
+
 app.get('/', (req, res) => {
 	res.send('Hello World !!' );
 });
+
 
 app.all('*', (req, res) => {
 	res.send('404 Not found');
