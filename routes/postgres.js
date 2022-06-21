6f3aa163b07f2dcff78ebfeaf23a5591821db013
YY <cafesenzanome2005@yahoo.co.jp>
2022-06-21 21:37:27 +0900

    a

--- routes/postgres.js
+++ routes/postgres.js
@@ -38,10 +38,16 @@ const pg = new Client({
 */
 //---------------------------------------
 
-const { Pool } = require('pg').Pool;
+const { Pool } = require('pg');
 //const connectionString = 'postgresql://postgres:postgres@localhost:5432/testdb';
 const connectionString = 'postgres://zxxrerpbjexjkt:d6376ad799cd1032ad56f03dd6d2583f65fdc2a29d3f283f9e7fe69d463e2de1@ec2-54-211-255-161.compute-1.amazonaws.com:5432/de90tuv7jdic7i';
-const pool = new Pool({ connectionString });
+//const pool = new Pool({ connectionString });
+const pool = new Pool({ 
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+ });
 
 /**
  * Postgresクラス
